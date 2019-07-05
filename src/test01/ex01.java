a3c5016ae231fe61aa5e19e7218781936067e0b1
강명준 <mj@gangmyeongjun-ui-MacBookPro.local>
2019-07-05 13:18:29 +0900

    Update ex01.java

--- src/test01/ex01.java
+++ src/test01/ex01.java
@@ -2,6 +2,8 @@ package test01;
 
 public class ex01 {
 	public static void main(String[] args) {
-		System.out.println("Hello Wolrd");
+		String str = "안녕하세요 나는 지금 카페에서";
+		String str2 = "커피 마시고 있어요";
+		System.out.println(str + str2);
 	}
 }
